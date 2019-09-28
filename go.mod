b7535ce1097601e60ed7c5d71e07b8f41cded82d
Murat Bastas <muratbsts@gmail.com>
2019-09-28 16:17:40 +0300

    Flags, custom interval, notification center access

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/earthquake
+module github.com/murat/earthquake
 
 go 1.13
 
@@ -7,9 +7,14 @@ require (
 	github.com/antchfx/htmlquery v1.0.0 // indirect
 	github.com/antchfx/xmlquery v1.0.0 // indirect
 	github.com/antchfx/xpath v1.0.0 // indirect
+	github.com/deckarep/gosx-notifier v0.0.0-20180201035817-e127226297fb // indirect
+	github.com/gen2brain/beeep v0.0.0-20190719094215-ece0cb67ca77
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
+	github.com/kardianos/service v1.0.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lestrrat-go/strftime v0.0.0-20190725011945-5c849dd2c51d
+	github.com/martinlindhe/notify v0.0.0-20181008203735-20632c9a275a
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/gen2brain/beeep"
 	"github.com/gocolly/colly"
+	"github.com/lestrrat-go/strftime"
 )
 
-type deprem struct {
-	Tarih    string
-	Saat     string
-	Enlem    float64
-	Boylam   float64
-	Derinlik float64
-	SiddetML float64
-	SiddetMW float64
-	SiddetMD float64
-	Yer      string
+type earthquake struct {
+	Date      string
+	Time      string
+	DateTime  time.Time
+	Latitude  float64
+	Longitude float64
+	Depth     float64
+	Md        float64
+	Ml        float64
+	Mw        float64
+	Yer       string
 }
 
-type depremler struct {
-	list []deprem
+var (
+	earthquakes []earthquake
+	day         string
+	hour        string
+	minute      string
+	duration    time.Duration
+	min         float64
+)
+
+func init() {
+	day, _ = strftime.Format(`%Y.%m.%d`, time.Now())
+	hour, _ = strftime.Format(`%H`, time.Now())
+	minute, _ = strftime.Format(`%M`, time.Now())
+
+	var period int
+	flag.IntVar(&period, "period", 5, "periodic interval")
+
+	var min int
+	flag.IntVar(&min, "min", 3, "min")
+	flag.Parse()
+
+	duration = time.Minute * time.Duration(period)
 }
 
 func main() {
-	depremList := depremler{}
+	log.Printf("[*] BDTİM sayfası her %s de bir kontrol edilecek.", duration)
+	run()
+	select {}
+}
+
+func run() {
 	c := colly.NewCollector()
 	c.OnHTML("body > pre", func(e *colly.HTMLElement) {
 		table := strings.Split(e.Text, "--------------")
@@ -35,39 +65,72 @@ func main() {
 		for _, row := range rows {
 			if len(row) > 0 {
 				parsed := regexp.MustCompile(`\s+`).Split(row, 10)
-				enlem, _ := strconv.ParseFloat(strings.Join(parsed[3:4], ""), 64)
-				boylam, _ := strconv.ParseFloat(strings.Join(parsed[4:5], ""), 64)
-				derinlik, _ := strconv.ParseFloat(strings.Join(parsed[5:6], ""), 64)
-				siddetmd, _ := strconv.ParseFloat(strings.Join(parsed[6:7], ""), 64)
-				siddetml, _ := strconv.ParseFloat(strings.Join(parsed[7:8], ""), 64)
-				siddetmw, _ := strconv.ParseFloat(strings.Join(parsed[8:9], ""), 64)
+
+				latitude, _ := strconv.ParseFloat(parsed[2], 64)
+				longitude, _ := strconv.ParseFloat(parsed[3], 64)
+				depth, _ := strconv.ParseFloat(parsed[4], 64)
+				md, _ := strconv.ParseFloat(parsed[5], 64)
+				ml, _ := strconv.ParseFloat(parsed[6], 64)
+				mw, _ := strconv.ParseFloat(parsed[7], 64)
+
+				dots := regexp.MustCompile(`\.`)
+				dateString := dots.ReplaceAllString(parsed[0], "-") + "T" + parsed[1] + "+30:00"
+				dateTime, _ := time.Parse(time.RFC3339, dateString)
 
 				spaces := regexp.MustCompile(`\s+`)
-				d := deprem{
-					Tarih:    fmt.Sprintf("%s", parsed[0]),
-					Saat:     fmt.Sprintf("%s", parsed[1]),
-					Enlem:    enlem,
-					Boylam:   boylam,
-					Derinlik: derinlik,
-					SiddetMD: siddetmd,
-					SiddetML: siddetml,
-					SiddetMW: siddetmw,
-					Yer:      spaces.ReplaceAllString(fmt.Sprintf("%s", parsed[9]), " "),
+				d := earthquake{
+					Date:      parsed[0],
+					Time:      parsed[1],
+					DateTime:  dateTime,
+					Latitude:  latitude,
+					Longitude: longitude,
+					Depth:     depth,
+					Md:        md,
+					Ml:        ml,
+					Mw:        mw,
+					Yer:       spaces.ReplaceAllString(parsed[8], " "),
 				}
-				depremList.list = append(depremList.list, d)
+				earthquakes = append(earthquakes, d)
 			}
 		}
 	})
 
+	log.Print("Sayfa ziyaret ediliyor.")
 	c.Visit("http://www.koeri.boun.edu.tr/scripts/lst4.asp")
 
-	fmt.Printf("%v", depremList.enBuyuk())
+	last := last(earthquakes)
+	biggers := biggers(last)
+
+	for _, x := range biggers {
+		beeep.Notify(
+			"BDTİM Uyarı "+x.Date+" "+x.Time,
+			fmt.Sprintf("%s de %v şiddetinde earthquake oldu.", x.Yer, x.Ml),
+			"assets/logo.gif",
+		)
+	}
+
+	time.AfterFunc(duration, run)
 }
 
-func (depremler depremler) enBuyuk() deprem {
-	sort.Slice(depremler.list, func(i, j int) bool {
-		return depremler.list[i].SiddetMD > depremler.list[j].SiddetMD
-	})
+func last(list []earthquake) []earthquake {
+	newlist := list[:0]
+	now := time.Now()
+	for _, x := range list {
+		if duration.Minutes() >= now.Sub(x.DateTime).Minutes() {
+			newlist = append(newlist, x)
+		}
+	}
 
-	return depremler.list[0]
+	return newlist
+}
+
+func biggers(list []earthquake) []earthquake {
+	newlist := list[:0]
+	for _, x := range list {
+		if x.Ml > min {
+			newlist = append(newlist, x)
+		}
+	}
+
+	return newlist
 }
